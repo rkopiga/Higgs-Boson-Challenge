a19d0fa8edd5cfcece56022db73881507739227f
Rayan DAOD <rayan.daodnathoo@epfl.ch>
2019-09-21 22:17:45 +0200

    Write the squeleton of the implementations file

---
+++ implementations.py
@@ -0,0 +1,33 @@
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+	"""
+	Linear regression using gradient descent.
+	"""
+	return None
+	
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+	"""
+	Linear regression using stochastic gradient descent.
+	"""
+	return None
+	
+	
+def least_squares(y, tx):
+	"""
+	Linear squares regression using normal equations.
+	"""
+	return None
+	
+
+def ridge_regression(y, tx, initial_w, max_iters, gamma):
+	"""
+	Ridge regression using normal equations.
+	"""
+	return None
+	
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+	"""
+	Regularized logistic regression using gradient descent.
+	"""
+	return None
