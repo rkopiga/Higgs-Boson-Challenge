34e9770a95f080dcab4c83fe22099bede16656bf
Kopiga Rasiah <kopiga.rasiah@epfl.ch>
2019-10-02 19:52:55 +0200

    completed last three regression functions

--- implementations.py
+++ implementations.py
@@ -19,27 +19,7 @@ def mean_absolute_error():
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 	"""
 	Linear regression using gradient descent.
-	
-	Parameters
-	----------
-	y: vector
-		The outputs
-	tx: vector
-		The inputs
-	initial_w: vector
-		The initial weight vector
-	max_iters: int
-		The number of steps to run
-	gamma: double
-		The step-size (>0)
-		
-	Returns
-	-------
-	tupple
-			(w, loss) the last weight vector of the method, 
-			and the corresponding loss value (cost function)
 	"""
-	
 	w_temp = initial_w
 	for i in range(max_iters):
 		loss, loss_gradient = mean_square_error(y, tx, w_temp)
@@ -53,12 +33,14 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
 	"""
     w_temp = initial_w
     for i in range(max_iters):
-        loss, loss_gradient = mean_square_error(y, tx, w_temp)
+        
         random_data_row = np.random(range(tx.shape[0]))
         e = (y - tx@w_temp)[random_data_row]
         Ln_grad = -2*e*tx[random_data_row]
         w_temp = w_temp - gamma * Ln_grad
-	return (w_temp, loss)
+    loss, _ = mean_square_error(y, tx, w_temp)
+    w = w_temp
+	return (w, loss)
 	
 	
 def least_squares(y, tx):
@@ -71,15 +53,45 @@ def least_squares(y, tx):
 	return (w, loss)
 	
 
-def ridge_regression(y, tx, initial_w, max_iters, gamma):
+def ridge_regression(y, tx, lamda_):
 	"""
 	Ridge regression using normal equations.
 	"""
-	return None
-	
+    gamma_prime = 2 * tx.shape[0] * lambda_
+    additional_matrix = np.ones(tx.shape[1])
+    tx_T = tx.T
+    w = linalg.inv(tx_T @ tx + additional_matrix) @ tx_T @ y
+    loss, loss_gradient = mean_square_error(y, tx, w)
+	return (w, loss)
+
+def logistic_regression_GD(y, tx, initial w, max_iters, gamma):
+    """
+    Logistic regression using gradient descent or SGD
+    """
+    w_temp = initial_w
+    for i in range(max_iters):
+        loss_gradient = tx.T @ (logistic_function(tx@w) - y)
+        w_temp = w_temp - gamma * loss_gradient
+    w = w_temp
+    loss, _ = mean_square_error(y, tx, w_temp)
+    return (w,loss)
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
 	"""
 	Regularized logistic regression using gradient descent.
 	"""
-	return None
+    w_temp = initial_w
+    for i in range(max_iters):
+        random_data_row = np.random(range(tx.shape[0]))
+        tx_n = tx[random_data_row]
+        y_n = y[random_data_row]
+        loss_gradient = tx_n * (logistic_function(np.dot(tx_n,y_n)) - y_n)
+        w_temp =  w_temp - gamma * loss_gradient
+    w = w_temp    
+    loss, _ = mean_square_error(y, tx, w)
+	return (w,loss)
+
+def logistic_function(z):
+    return np.exp(z)/(1 + np.exp(z))
+
+
