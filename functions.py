4ee9af012aaf53cbb569b9f325f43a0d89c43a04
Yann Meier <yann.meier@epfl.ch>
2019-10-04 21:37:14 +0200

    creating folders + importing data from project description

---
+++ functions.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def load_data(path_dataset):
+    """Load data and convert it to the metrics system."""
+    data = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[range(32)])
+    return data
